fdce88a55a86c67e0756b0665effdf22959dbe63
luis <luis.diaz.car@gmail.com>
2020-07-01 23:25:53 -0500

    add bootstrap config to this project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
+// npm packages
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// local
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Bootstrap config
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Use BootstrapVue
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
