da48dfb15e64a6aaa8f44fb823e59cafefef3d35
luis <luis.diaz.car@gmail.com>
2020-07-02 19:02:10 -0500

    implementation of loading and layouts App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <LoadLayout v-if="isLoading">
+      <BaseLoading/>
+    </LoadLayout>
+
+    <MainLayout v-else/>
+
   </div>
 </template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+import LoadLayout from './layouts/LoadLayout'
+import MainLayout from './layouts/MainLayout'
+import BaseLoading from '@/components/BaseLoading.vue'
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+    LoadLayout,
+    BaseLoading
+  },
+  computed: {
+    ...mapState('loading', { isLoading: 'isLoading' })
+  }
+}
+</script>
 
 <style lang="stylus">
   #app
