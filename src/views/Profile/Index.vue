db5d4f0c9cadff4d9bc9b25ad869ba2a71d1f7c7
luis <luis.diaz.car@gmail.com>
2020-07-04 12:46:42 -0500

    calling api to get profile data  from profile view

--- src/views/Profile/Index.vue
+++ src/views/Profile/Index.vue
@@ -1,11 +1,56 @@
 <template>
     <div>
+        <BaseLoading v-if="isLoading"/>
         <h1>Profile Index</h1>
     </div>
 </template>
 
 <script>
+
+// Import mixin error to handle api response error
+import setError from '@/mixins/setError'
+import { getApiAccount } from '@/api/search'
+import BaseLoading from '@/components/BaseLoading'
+
 export default {
-  name: 'ProfileView'
+  name: 'ProfileView',
+  mixins: [
+    setError
+  ],
+  components: { BaseLoading },
+  data () {
+    return {
+      isLoading: false,
+      profileData: null
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      const { region, battleTag: account } = this.$route.params
+      getApiAccount({ region, account })
+        .then(({ data }) => {
+          this.profileData = data
+          console.log(this.profileData)
+        })
+        .catch((err) => {
+          this.profileData = null
+          const errObj = {
+            routeParams: this.$route.params,
+            message: err.message
+          }
+          if (err.response) {
+            errObj.data = err.response.data
+            errObj.status = err.response.status
+          }
+          // using error mixin
+          this.setApiErr(errObj)
+          // go to 'Error' route
+          this.$router.push({ name: 'Error' })
+        })
+    }
+  }
 }
 </script>
